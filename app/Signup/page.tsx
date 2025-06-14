e7cdf38b72d4c826cc0b362d7d694385119b7b35
Chrismugwimi <chrismugwimi01@gmail.com>
2025-06-14 23:59:01 +0300

    sign up

--- app/Signup/page.tsx
+++ app/Signup/page.tsx
@@ -89,7 +89,7 @@ export default function SignUpPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-blue-300 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         {/* Back to home */}
         <Link
